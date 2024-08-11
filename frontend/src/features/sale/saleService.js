82c6824a8bd006c1977fc9feb49c93e0b2fd6d81
sudalaiyandi2004 <117078836+sudalaiyandi2004@users.noreply.github.com>
2024-08-11 13:49:28 +0530

    Update saleService.js

--- frontend/src/features/sale/saleService.js
+++ frontend/src/features/sale/saleService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const API_URL = '/api/sales/'
-const API_URLS = '/api/sales/buy/'
-const API_URLSS = '/api/sales/driver/'
+const API_URL = 'https://vendor-connect.onrender.com/api/sales/'
+const API_URLS = 'https://vendor-connect.onrender.com/api/sales/buy/'
+const API_URLSS = 'https://vendor-connect.onrender.com/api/sales/driver/'
 // Create new goal
 const createSales = async (goalData, token) => {
     const config = {
